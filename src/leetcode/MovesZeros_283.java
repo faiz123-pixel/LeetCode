ed513b4a02ecc057d7183ac5c660d7c6caaed07f
faiz123-pixel <fa123izansari@gmail.com>
2026-07-18 00:55:10 +0530

    solutions problem 283 on leetcode

---
+++ src/leetcode/MovesZeros_283.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class MovesZeros_283 {
+	public static void main(String[] args) {
+		
+		int[] nums = {0,1,0,3,12};
+		int j=0;
+		for (int i=0;i<nums.length;i++) {
+			if(nums[i]!=0) {
+				
+				int temp=nums[j];
+				nums[j]=nums[i];
+				nums[i]=temp;
+				j++;
+			}
+		}
+	}
+}
