7daa545ddf3709939d341c805b15d73011d1c4ec
faiz123-pixel <fa123izansari@gmail.com>
2026-08-08 01:04:42 +0530

    New solution added

--- src/leetcode/LongestCommonPrefix_14.java
+++ src/leetcode/LongestCommonPrefix_14.java
@@ -2,7 +2,22 @@ package leetcode;
 
 public class LongestCommonPrefix_14 {
 	public static void main(String[] args) {
-		
+//		  if(strs ==null || strs.length==0){
+//	            return "";
+//	        }
+//	        Arrays.sort(strs);
+//	        String first=strs[0];
+//	        String last=strs[strs.length-1];
+//	        int i=0;
+//	        while(i<first.length()){
+//	            if(first.charAt(i)==last.charAt(i)){
+//	                i++;
+//	            }
+//	            else{
+//	                break;
+//	            }
+//	        }
+//	        return i==0?"":first.substring(0,i);
 	}
 
 }
